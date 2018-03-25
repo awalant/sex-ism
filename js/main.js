fd8fd3974a3065a45923f654a0a014794060acd9
Anna Walant <>
2018-03-25 15:39:21 -0400

    Update commenting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 "use strict"
 
 
+/*General basis of this code from Jonathan Soma, via https://www.youtube.com/watch?v=lPr60pexvEM and https://www.youtube.com/watch?v=NTS7uXOxQeM
+Data from http://sexualitics.github.io/ */
+
+//Sets the dimensions of the canvas
 let width = 1200;
 let height = 600;
 
+//Attaches an SVG canvas to the div id canvas. leaves the chart starting at the top left of the window and attaches a group to it.
 let canvas = d3.select("#canvas")
     .append("svg")
     .attr("height", height)
@@ -11,10 +16,11 @@ let canvas = d3.select("#canvas")
     .append("g")
     .attr("transform", "translate(0,0)");
 
-
-let rScale = d3.scaleSqrt().domain([0, 30031])
+//Sets the scale for the radius. Using a square root scale because they are circles, and the domain goes from the min to the max of the data.
+let rScale = d3.scaleSqrt().domain([0, 25526])
     .range([2, 50]);
 
+//groupedX is a variable for storing the x force of the simulation for when the bubbles are divided into their categories. It places each of the different categories at separate spots on the x axis.
 let groupedX = d3.forceX((d) => {
     if (d.category === "female_anatomy") {
         return 200;
@@ -29,22 +35,31 @@ let groupedX = d3.forceX((d) => {
     }
 }).strength(.3);
 
+
+//centralX is a variable for storing the x force of the simulation for when the bubbles are meant to be clumped in the center of the canvas together.
 let centralX = d3.forceX(600).strength(.05);
 
+//dontTouchMe is a variable for setting the distance between each bubble. It uses the same calculation for the radius (above) and adds 2 px to it.
 let dontTouchMe = d3.forceCollide((d) => {
     return rScale(d.occurances) + 2;
 });
 
+//A force simulation for when the bubbles first load. They all start in the center.
 let sim = d3.forceSimulation()
     .force("centralX", centralX)
     .force("centralY", d3.forceY(height / 2).strength(.05))
     .force("dontTouchMe", dontTouchMe)
 .restart();
-//.alphaDecay([.01]);
 
+//Everything that uses the data goes here:
+
+//Converts the data from a csv and gives it the temporary variable pornData. 
 let data = d3.csv("data/the_data.csv", (error, pornData) => {
+    
+    //if there is an error, this will be a notification
     if (error) throw error;
 
+    //Puts the circles in the body of index.html and uses the pornData to apply to each circle. It initializes it, each one getting a circle, gives them the class .terms, and says that for each instance of a circle the radius will be determined by the rScale and how many times the word was used. They are filled blue, and when they are clicked on they return the data of that instance to the console.
     let circles = canvas.selectAll("body")
         .data(pornData)
         .enter().append("circle")
@@ -58,6 +73,7 @@ let data = d3.csv("data/the_data.csv", (error, pornData) => {
         });
 
 
+    //when the button for term is clicked, it (should) force the bubbles back to the center.
     d3.select("#term").on("click", () => {
         sim
             .force("centralX", centralX)
@@ -65,6 +81,7 @@ let data = d3.csv("data/the_data.csv", (error, pornData) => {
             .restart();
     });
 
+    //when the button for categories is clicked, it should spread the groups apart from one another.
     d3.select("#cat").on("click", () => {
         sim
             .force("groupedX", groupedX)
@@ -72,9 +89,11 @@ let data = d3.csv("data/the_data.csv", (error, pornData) => {
             .restart();
     });
 
+    //This specifies for the nodes to move at each tick (automatically applied in a force simulation)
     sim.nodes(pornData)
         .on("tick", ticked);
 
+    //X and Y coordinates are determined by this function.
     function ticked() {
         circles
             .attr("cx", (d) => {
@@ -87,241 +106,3 @@ let data = d3.csv("data/the_data.csv", (error, pornData) => {
     }
 
 });
-
-
-
-
-
-
-
-
-
-//let bubbleChart;
-//
-//let chart = bubbleChart();
-//
-//bubbleChart = ()=>{
-//    let width = 600;
-//    let height = 400;
-//    
-//    chart.width = (value)=>{
-//        if (!arguments.length){
-//            return width;
-//        }
-//        width = value;
-//        return chart;
-//    }
-//    
-//    chart.height = (value)=>{
-//        if (!arguments.length){
-//            return height;
-//            }
-//        height = value;
-//        return chart;
-//    }
-//    return chart;
-//}
-//
-//
-//d3.csv("data/the_data.csv", (error, pornData)=>{
-//    if (error){
-//        console.error("the data hasn't worked...");
-//        throw error;
-//    }
-//    
-//    let data = pornData;
-//    
-//    let chart = bubbleChart().width(600).height(400);
-//    
-//    let div = d3.select("body")
-//    .selectAll("div")
-//    .data(data)
-//    .enter()
-//    .append("div");
-//    d3.select("#canvas").data(data).call(chart);
-//    console.log(data);
-//    
-//    
-//     
-//})
-
-
-
-
-
-
-
-
-
-//var canvas = d3.select("#canvas"),
-//    width = +canvas.attr("width"),
-//    height = +canvas.attr("height");
-//
-//var format = d3.format(",d");
-//
-//var color = d3.scaleSequential(d3.interpolateMagma)
-//    .domain([-4, 4]);
-//
-////var stratify = d3.stratify()
-////    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
-//
-//var pack = d3.pack()
-//    .size([width - 2, height - 2])
-//    .padding(3);
-//
-//var pornData = d3.json("data/hierarchy.json", function(error, data) {
-//  if (error) throw error;
-//    let root = pornData[0];
-// 
-//
-//  pack(root);
-//
-//  var node = canvas.select("g")
-//    .selectAll("g")
-//    .data(root.descendants())
-//    .enter().append("g")
-//      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-//      .attr("class", function(d) { return "node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root"); })
-//      .each(function(d) { d.node = this; })
-//      .on("mouseover", hovered(true))
-//      .on("mouseout", hovered(false));
-//
-//  node.append("circle")
-//      .attr("id", function(d) { return "node-" + d.id; })
-//      .attr("r", function(d) { return d.r; })
-//      .style("fill", function(d) { return color(d.depth); });
-//
-//  var leaf = node.filter(function(d) { return !d.children; });
-//
-//  leaf.append("clipPath")
-//      .attr("id", function(d) { return "clip-" + d.id; })
-//    .append("use")
-//      .attr("xlink:href", function(d) { return "#node-" + d.id + ""; });
-//
-//  leaf.append("text")
-//      .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-//    .selectAll("tspan")
-//    .data(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g); })
-//    .enter().append("tspan")
-//      .attr("x", 0)
-//      .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-//      .text(function(d) { return d; });
-//
-//  node.append("title")
-//      .text(function(d) { return d.id + "\n" + format(d.value); });
-//});
-//
-//function hovered(hover) {
-//  return function(d) {
-//    d3.selectAll(d.ancestors().map(function(d) { return d.node; })).classed("node--hover", hover);
-//  };
-//}
-//
-
-
-
-
-
-
-
-
-
-//let margin = {
-//    left: 100,
-//    right: 100,
-//    top: 200,
-//    bottom: 100
-//};
-//
-//let width = 800 - margin.left - margin.right,
-//    height = 600 - margin.top - margin.bottom;
-//
-//
-//let pack = d3.pack()
-//    .size([width, height])
-//    .padding(2);
-//
-//let pornData = d3.json("data/hierarchy.json", function (error, pornData) {
-//    if (error) throw error;
-//
-//    root = pornData[0];
-//    console.log(root);
-//    pack(root);
-//
-//    let node = svg.select("g")
-//        .selectAll("g")
-//        .data(root.descendants())
-//        .enter().append("g")
-//        .attr("transform", function (d) {
-//            return "translate(" + d.x + "," + d.y + ")";
-//        })
-//        .attr("class", function (d) {
-//            return "node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root");
-//        });
-//        //    })
-////        .each(function (d) {
-////            d.node = this;
-////        })
-////        .on("mouseover", hovered(true))
-////        .on("mouseout", hovered(false));
-//
-//
-//});
-//
-//
-//let flag = true;
-//
-//let trans = d3.transition().duration(750);
-//
-//let g = d3.select("#canvas")
-//    .append("svg")
-//    .attr("width", width + margin.left + margin.right)
-//    .attr("height", height + margin.top + margin.bottom)
-//    .append("g")
-//    .attr("transform", "translate(" + margin.left + margin.top + ")");
-//
-//
-
-
-
-
-
-
-
-////let data = d3.csv("data/the_data.csv", function(error, data) {
-////    if (error) throw error;
-//    
-////    let root = d3.stratify(data);
-////    console.log(root);
-////});
-//
-//let stratify = d3.stratify()
-//.parentId(function(d){
-//    return d.id.substring(0, d.id.lastIndexOf(".")); });
-//
-//
-//
-//
-//let data = d3.csv("data/the_data.csv", function (data) {
-//    console.log(data);
-//     data.forEach(function (d) {
-//            d.occurances = +d.occurances;
-//        });
-//    
-//    let root = stratify(data)
-//    .sum(function(d){
-//        return d.value;
-//    })
-//    .sort(function(a,b){
-//        return b.value - a.value;
-//    });
-//    
-////    let root = d3.hierarchy(data)
-////    .sum(function(d){
-////      return d.occurances;  
-////    })
-////    .sort(function(a,b){
-////        return b.occurances - a.occurances;
-////    });
-//    console.log(root);
-//});
